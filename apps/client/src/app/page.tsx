2621b89064e1a66407d18e7f2d99ed6392ae8715
seoleunjin <anah9026@gmail.com>
2026-02-23 17:39:12 +0900

    chore(ui-foundation): 디자인 시스템 추가 또는 수정
    - 디자인 토큰 추가 또는 수정
    - 유틸리티 추가 및 수정
    - fonts 파일 프리텐다드 추가

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Page() {
   return (
     <div>
-      <div className="p-6 text-brand-700 font-bold">tailwind admin ok</div>
+      <div className="p-6 title2">
+        <p className="title2">낮은 가격순</p>
+      </div>
+      <input type="text" className="field " placeholder="test" />
+      <input type="text" className="field" disabled placeholder="test" />
+      <input type="text" className="field field-error " placeholder="test" />
     </div>
   );
 }
