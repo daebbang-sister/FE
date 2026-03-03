5438c0ce54019c6000c8f08b3c974ff0866566fb
seoleunjin <anah9026@gmail.com>
2026-03-03 18:10:12 +0900

    feat(ui): 드롭다운 컴포넌트에 상태에 따른 스타일링 추
    - Headless UI 기반 드롭다운 컴포넌트 구현
    - 드롭다운 전체 및 옵션 단위 disabled 상태 지원

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,11 +1,51 @@
-import { CheckBox } from "packages/ui/src";
+import { Dropdown } from "packages/ui/src";
 
 export default function Page() {
+  const phoneOptions = [
+    { label: "010", value: "010" },
+    { label: "011", value: "011" },
+    { label: "016", value: "016", disabled: true },
+    { label: "017", value: "017" },
+    { label: "018", value: "018" },
+    { label: "019", value: "019" },
+  ];
+
   return (
-    <div className="max-w-sm space-y-6 p-12">
-      <div className="flex items-center gap-1.5">
-        <CheckBox id="agree" />
-        <label htmlFor="agree">동의합니다.</label>
+    <div>
+      <div className=" w-3xl space-y-6">
+        {/* ① 전체 disabled */}
+        <Dropdown
+          name="phonePrefixAllDisabled"
+          placeholder="선택"
+          options={phoneOptions}
+          status="disabled"
+          size="L"
+        />
+
+        {/* ② 016만 disabled */}
+        <Dropdown
+          name="phonePrefixPartialDisabled"
+          placeholder="선택"
+          options={phoneOptions}
+          size="L"
+        />
+
+        <Dropdown
+          name="phonePrefixPartialDisabled"
+          placeholder="사이즈를 선택해주세요."
+          options={phoneOptions}
+          size="L"
+        />
+      </div>
+      <div className=" w-sm space-y-6 ml-auto">
+        {/* 정렬 */}
+        <Dropdown
+          name="phonePrefixPartialDisabled"
+          defaultValue="정렬"
+          options={phoneOptions}
+          size="M"
+          className="w-20 ml-auto"
+        />
       </div>
     </div>
   );
