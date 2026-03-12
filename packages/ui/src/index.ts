e56df57980b64c3a3853e6411229284a66228649
강도현 <kdh0063@gachita.kr>
2026-03-13 00:08:00 +0900

    feat: 공통 컴포넌트 페이지버튼 설계
    - 현재 페이지 강조 표시 (active)
    - 페이지당 데이터 개수 설정 (pageSize)
    - 페이지 변경 시 스크롤 위치 처리
    - 재사용 가능하도록 props 설계
    - 한 번에 보여줄 페이지 개수 설정 (ex: 5개 / 10개)
    - 페이지 이동 버튼 / startPage, endPage 계산 로직 구현

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,5 +2,6 @@ export { cn } from "./lib/utils";
 export { Input } from "./input/Input";
 export { Button } from "./button/Button";
 export { TabButton } from "./button/TabButton";
+export { PageButton } from "./button/PageButton";
 export { CheckBox } from "./checkbox/CheckBox";
 export { Dropdown } from "./dropdown/Dropdown";
