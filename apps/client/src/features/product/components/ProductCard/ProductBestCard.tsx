db408f08f802d3878e7f4ffb7ccb4e6e0ffe4665
강도현 <kdh0063@gachita.kr>
2026-03-24 17:54:54 +0900

    feature: 베스트 상품카드 컴포넌트 제작
    - 베스트 상품카드 컴포넌트 제작

---
+++ apps/client/src/features/product/components/ProductCard/ProductBestCard.tsx
@@ -0,0 +1,57 @@
+import { Badge, ProductBestImage } from "@/shared/ui";
+import ProductColorChips from "./ProductColorChips";
+import Link from "next/link";
+
+type ProductBestCardProps = {
+  id: number;
+  category: string;
+  colors: string[];
+  title: string;
+  originalPrice: string;
+  salePrice?: string;
+  discount?: string;
+  primaryImage: string;
+};
+
+export default function ProductBestCard({
+  id,
+  category,
+  colors,
+  title,
+  originalPrice,
+  salePrice,
+  discount,
+  primaryImage,
+}: ProductBestCardProps) {
+  return (
+    <Link
+      href={`/products/${category}/${id}`}
+      className="group block cursor-pointer"
+    >
+      <ProductBestImage primaryImage={primaryImage} />
+      <h3 className="mt-6 mb-4 truncate">{title}</h3>
+
+      {salePrice ? (
+        <div>
+          <p className="caption1 text-text-disabled mb-1.5 line-through">
+            {originalPrice}
+          </p>
+          <div className="flex items-center gap-1.25">
+            <p className="caption1">{salePrice}</p>
+            <Badge>{discount}</Badge>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p className="caption1">{originalPrice}</p>
+        </div>
+      )}
+
+      {colors && (
+        <div className="mt-4">
+          <ProductColorChips colors={colors} />
+        </div>
+      )}
+    </Link>
+  );
+}
