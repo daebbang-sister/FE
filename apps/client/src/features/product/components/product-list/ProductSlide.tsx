20d2745dbe4f858a4789695b961264e2f1f03d66
강도현 <kdh0063@gachita.kr>
2026-03-25 13:31:55 +0900

    feature: 상품리스트 페이지 작업
    - 베스트 상품 카드 컴포넌트 분리 및 구현
    - embla-carousel-react를 사용하여 베스트 상품 슬라이드 구현
    - 상품 클릭 시 상세페이지 이동 처리

---
+++ apps/client/src/features/product/components/product-list/ProductSlide.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { MainProduct } from "@/features/home/model";
+import ProductBestCard from "@/features/product/components/ProductCard/ProductBestCard";
+import useEmblaCarousel from "embla-carousel-react";
+
+type Props = {
+  products: MainProduct[];
+};
+
+export default function ProductSlide({ products }: Props) {
+  const [emblaRef] = useEmblaCarousel({
+    loop: false,
+    align: "start",
+    dragFree: true,
+  });
+
+  return (
+    <div ref={emblaRef} className="mb-12 w-full overflow-hidden">
+      <div className="flex gap-5">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="flex-[0_0_42%] md:flex-[0_0_29%] lg:flex-[0_0_17%]"
+          >
+            <ProductBestCard
+              id={product.id}
+              category={product.categoryName}
+              title={product.productName}
+              primaryImage={product.mainImageUrl}
+              originalPrice={`${product.originalPrice.toLocaleString()}원`}
+              salePrice={
+                product.discountRate
+                  ? `${product.sellingPrice.toLocaleString()}원`
+                  : undefined
+              }
+              discount={
+                product.discountRate ? `${product.discountRate}%` : undefined
+              }
+              colors={[]}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
