19c821bec2f27913a559d8404240b90cf88592f8
강도현 <kdh0063@gachita.kr>
2026-04-08 20:01:41 +0900

    feat: 상품 상세페이지 제작
    - 상품 상세 api연결
    - 상품 ProdcutCarousel 메인 이미지 추가
    - 상품 찜하기 연결 준비
    - 상품 장바구니 연결 준비
    - 상품 결제 연결 준비

--- apps/client/src/app/products/[category]/[productId]/page.tsx
+++ apps/client/src/app/products/[category]/[productId]/page.tsx
@@ -1,3 +1,4 @@
+import { getProductDetail } from "@/features/product/api";
 import ProdcutCarousel from "@/features/product/components/product-detail/ProdcutCarousel";
 import ProductDetailSummary from "@/features/product/components/product-detail/ProductDetailSummary";
 import ProductDetailTab from "@/features/product/components/product-detail/ProductDetailTab";
@@ -11,6 +12,8 @@ type Props = {
 
 export default async function ProductDetailPage({ params }: Props) {
   const { category, productId } = await params;
+  const productDetail = await getProductDetail(Number(productId));
+  console.log("제품 정보", productDetail);
   {
     /* <p>category: {category}</p>
   <p>productId: {productId}</p> */
@@ -21,17 +24,19 @@ export default async function ProductDetailPage({ params }: Props) {
       <div className="grid grid-cols-1 gap-y-6.25 lg:gap-x-14.5 lg:gap-y-6.25">
         <div className="min-w-0 lg:col-start-1 lg:row-start-1">
           <ProdcutCarousel
-            images={[
-              "/images/look1.jpg",
-              "/images/look2.jpg",
-              "/images/look3.jpg",
-              "/images/look4.jpg",
-              "/images/look5.jpg",
-            ]}
+            mainImages={productDetail.mainImageUrl}
+            gallery={productDetail.gallery}
           />
         </div>
         <aside className="lg:sticky lg:top-39.5 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:self-start">
-          <ProductDetailSummary />
+          <ProductDetailSummary
+            productName={productDetail.productName}
+            simpleDescription={productDetail.simpleDescription}
+            originalPrice={productDetail.originalPrice}
+            sellingPrice={productDetail.sellingPrice}
+            discountRate={productDetail.discountRate}
+            options={productDetail.options}
+          />
         </aside>
         <div className="min-w-0 lg:col-start-1 lg:row-start-2">
           <ProductDetailTab />
--- apps/client/src/features/product/model.ts
+++ apps/client/src/features/product/model.ts
@@ -18,3 +18,37 @@ export type CategoryProduct = {
   discountRate: number | null;
   colorCodes: string[];
 };
+
+// #############################
+
+export type ProductSize = {
+  productDetailId: number;
+  size: string;
+  stock: number;
+  soldOut: boolean;
+};
+
+export type ProductOption = {
+  color: string;
+  colorCode: string;
+  sizes: ProductSize[];
+};
+
+export type ProductGallery = {
+  imageUrl: string;
+  imageOrder: number;
+};
+
+export type ProductDetail = {
+  id: number;
+  categoryName: string;
+  productName: string;
+  simpleDescription: string;
+  mainImageUrl: string;
+  originalPrice: number;
+  sellingPrice: number;
+  discountRate: number;
+  gallery: ProductGallery[];
+  descriptionHtml: string;
+  options: ProductOption[];
+};
